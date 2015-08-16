ec3afe35aed86fa0a14dc929246e7863308874c1
Marco.Silipo <m.silipo@web.de>
2015-08-16 15:12:51 +0200

    Implemented #64

--- userconfig/X39/extensions/X39_MS2_NativeQuickActions.hpp
+++ userconfig/X39/extensions/X39_MS2_NativeQuickActions.hpp
@@ -10,6 +10,7 @@ class X39_MS2_NativeQuickActions : extensionBase
 				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_PutNasopharyngeal.sqf",
 				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_TakeKingLT.sqf",
 				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_TakeNasopharyngeal.sqf",
+				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_UseBagvalveMask.sqf",
 				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\bandageAll.sqf",
 				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\dropTourniquetAll.sqf",
 				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\medipackAll.sqf",
