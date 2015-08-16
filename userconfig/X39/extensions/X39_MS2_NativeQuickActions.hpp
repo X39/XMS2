54d32d76a14c3386c6fbffb93ab0a70c69c54f43
Marco.Silipo <m.silipo@web.de>
2015-08-16 15:02:12 +0200

    Implemented #60

--- userconfig/X39/extensions/X39_MS2_NativeQuickActions.hpp
+++ userconfig/X39/extensions/X39_MS2_NativeQuickActions.hpp
@@ -6,8 +6,10 @@ class X39_MS2_NativeQuickActions : extensionBase
 	files[] = {
 				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_GiveSaline.sqf",
 				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_Heatpack.sqf",
-				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_TakeNasopharyngeal.sqf",
+				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_PutKingLT.sqf",
 				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_PutNasopharyngeal.sqf",
+				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_TakeKingLT.sqf",
+				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_TakeNasopharyngeal.sqf",
 				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\bandageAll.sqf",
 				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\dropTourniquetAll.sqf",
 				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\putTourniquetAll.sqf"
