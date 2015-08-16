27610d3fb07ac3dd5caed888aa2ff22729051f5b
Marco.Silipo <m.silipo@web.de>
2015-08-16 14:57:24 +0200

    Missing references for recently added QuickActions

--- userconfig/X39/extensions/X39_MS2_NativeQuickActions.hpp
+++ userconfig/X39/extensions/X39_MS2_NativeQuickActions.hpp
@@ -4,7 +4,13 @@ class X39_MS2_NativeQuickActions : extensionBase
 	requiredExtensions[] =  {"X39_MS2_NativeMedicalActions"};
 	version = "1.0";
 	files[] = {
-				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\bandageAll.sqf"
+				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_GiveSaline.sqf",
+				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_Heatpack.sqf",
+				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_TakeNasopharyngeal.sqf",
+				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\ACE3_PutNasopharyngeal.sqf",
+				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\bandageAll.sqf",
+				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\dropTourniquetAll.sqf",
+				"\userconfig\X39\extensions\X39_MS2_NativeQuickActions\putTourniquetAll.sqf"
 			  };
 	serverOnly = 0;
 };
